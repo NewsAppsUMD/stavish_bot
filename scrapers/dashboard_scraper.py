39413a2264727c8967805dabd774fc47822e1e0a
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-03-15 19:39:08 +0000

    add bs4 parser

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 url = 'https://diversity.umd.edu/black-student-leaders'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = response.content
-print(html)
+
+soup = BeautifulSoup(html, features="html.parser")
+print(soup.prettify())
