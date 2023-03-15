d36b3a4e7ba771154b015a788509a98b07522aa8
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-03-15 20:00:38 +0000

    find the first data point

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -6,5 +6,5 @@ response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = response.content
 
 soup = BeautifulSoup(html, features="html.parser")
-heading = soup.find(data-issue-header = "title")
+heading = soup.find(data-issue-header == "title")
 print(heading)
