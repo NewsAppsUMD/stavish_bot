bc11f5a9da1dcd864bd5790891e858a2994427d4
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-03-15 19:51:47 +0000

    targetting data-issue-header

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -6,5 +6,5 @@ response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = response.content
 
 soup = BeautifulSoup(html, features="html.parser")
-table = soup.find('h3.small.san.serif')
+table = soup.find('data-issue-header')
 print(table.prettify())
