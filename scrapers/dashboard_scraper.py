5e64eb02ea5c633ed7a6dc0ee07adb864e87a776
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-03-15 20:57:57 +0000

    loop for main

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -10,7 +10,7 @@ heading = soup.find_all(id = "main")
 number = []
 for elem in heading:
   #finding the < a > tag
-   cards = elem.find_all("data-issues-cards")
+   cards = elem.find_all("issues-card")
 #getting the text inside the < a > tag
 for i in cards:
   number.append(i.string)
