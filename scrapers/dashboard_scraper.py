1379054601f80a0031fe6189cc7e30422104a3ce
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-03-15 19:35:42 +0000

    starting from step 1

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -5,16 +5,3 @@ url = 'https://diversity.umd.edu/black-student-leaders'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = response.content
 print(html)
-
-soup = BeautifulSoup(html, features="html.parser")
-table = soup.find('tbody')
-
-list_of_rows = []
-for row in table.find_all('tr'):
-    list_of_cells = []
-    for cell in row.find_all('td'):
-        text = cell.text.strip()
-        list_of_cells.append(text)
-    list_of_rows.append(list_of_cells)
-
-print(list_of_rows)
