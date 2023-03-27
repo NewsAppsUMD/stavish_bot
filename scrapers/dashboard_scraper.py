d17963d704407159fc8a71bf9ab6590bc9bff2b4
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-03-27 20:30:50 +0000

    scraping divs instead of ids

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -6,12 +6,14 @@ response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 html = response.content
 
 soup = BeautifulSoup(html, features="html.parser")
-heading = soup.find_all(id = "main")
-number = []
-for elem in heading:
+#heading = soup.find_all(id = "main")
+#number = []
+#for elem in heading:
   #finding the < a > tag
-   cards = elem.find_all("div")
+   #cards = elem.find_all("div")
 #getting the text inside the < a > tag
-for i in cards:
-  number.append(i.string)
-print(number)
+#for i in cards:
+  #number.append(i.string)
+#print(number)
+for i in soup.select('div.data-issue'):
+    print(i.string)
