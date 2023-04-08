14f5c18ae451c3d0e86ebfeeca9052a89d7febc4
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 19:13:23 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -19,7 +19,7 @@ for div in divs:
 
 # Extract the partners from the <div> element with data-issue="partners"
     partners_soup = soup.find_all("div", {"data-issue": "partners"})
-    partners = for partner in partners_soup.find_all("li")
+    partners = partners_soup.find_all("li")
 
 print("Issue:", issue)
 print("Title:", title)
