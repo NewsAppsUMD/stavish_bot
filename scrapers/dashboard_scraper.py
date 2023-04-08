9e67f4f902d5b8e7cdbaed6488bd9ffb5a180f38
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 22:42:04 +0000

    actions

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -34,7 +34,9 @@ with open('25_demands_table.csv', 'w', newline='') as csvfile:
         update = [time.text.strip() for time in update_div.find_all("time")]
         actions = []
         actions_div = div.find("div", {"data-modal": "body"})
-        actions = [p.text.strip() for p in actions_div.find_all("p")]
+        if actions_div is not None:
+            actions = [p.text.strip() for p in actions_div.find_all("p")]
+
 
 
         issues_text = ', '.join([issue.strip() for issue in issues])
