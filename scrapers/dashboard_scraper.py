731dbfc0b18f02a18caf54642edb07d5a29562b9
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 15:16:40 +0000

    first web scraper

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -2,11 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import urllib.request
+from pprint import pprint
 
 url = 'https://diversity.umd.edu/black-student-leaders'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = response.content
-print(html)
 
-soup = BeautifulSoup(html, features="html.parser")
-print(soup.prettify())
+data = requests.get(url)
+
+print(data.text)
+
