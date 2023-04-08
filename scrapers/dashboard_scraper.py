22e2f97cbd13ca57b3fadf874fd28404700598a7
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 18:42:45 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import time
 import urllib.request
 from pprint import pprint
+import csv
 
 url = 'https://diversity.umd.edu/black-student-leaders'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -10,6 +11,10 @@ response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 soup = BeautifulSoup(response.content, "html.parser")
 
 divs = soup.find_all("div", {"data-card": "details"})
+
+    # Loop through each div and extract the desired information
+    for div in divs:
+        issue = div.find("data-issue=").text.strip()
 
 print(divs)
 
