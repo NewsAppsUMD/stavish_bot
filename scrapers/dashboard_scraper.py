32ef0c0acbde4c388c61f866744ecc93fbb2a69f
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 15:28:58 +0000

    first web scraper

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -8,23 +8,33 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-url = 'https://notes.ayushsharma.in/technology'
+url = 'https://diversity.umd.edu/black-student-leaders'
+response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+
 data = requests.get(url)
+
 
 my_data = []
 
+
 html = BeautifulSoup(data.text, 'html.parser')
-articles = html.select('a.post-card')
+articles = html.select('.list')
+
 
 for article in articles:
 
-    title = article.select('.card-title')[0].get_text()
-    excerpt = article.select('.card-text')[0].get_text()
-    pub_date = article.select('.card-footer small')[0].get_text()
+
+    title = article.select('.header')[0].get_text()
+    excerpt = article.select('.small-san-serif')[0].get_text()
+    pub_date = article.select('.extra-small-san-serif')[0].get_text()
+
 
     my_data.append({"title": title, "excerpt": excerpt, "pub_date": pub_date})
 
+
 pprint(my_data)
+
 
 
 
