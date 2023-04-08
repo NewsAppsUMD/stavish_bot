7b879022a08aba4a69a695a9ab5e2704defe6e22
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 21:16:44 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -12,26 +12,38 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 divs = soup.find_all("div", {"data-card": "details"})
 
-for div in divs:
-    issue = ("p", {"data-issue-header": "index"})
-    all_issue = [issue.text for issue in issue]
-    all_issue[0:25]
-    title = soup.find_all("h3", {"data-issue-header": "title"})
-    partners = soup.find_all("div", {"data-issue": "partners"})
-    partners_li = soup.find_all("ul")
-    #this status one needs help
-    status = soup.find_all("p", {"data-issue-status": "icon"})
-    status_span = soup.find_all("span")
-    updated = soup.find_all("div", {"data-issue": "date"})
-    actions = soup.find_all("div", {"data-modal": "body"})
+with open('25_demands_table.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Issue', 'Title', 'Partner', 'Status', 'Updated', 'Actions'])
 
+    for div in divs:
+        issues = []
+        for issue in soup.find_all("p", {"data-issue-header": "index"}):
+            issues.append(issue.text.strip())
+        title = soup.find_all("h3", {"data-issue-header": "title"})
+        partners = soup.find_all("div", {"data-issue": "partners"})
+        partners_li = soup.find_all("ul")
+        #this status one needs help
+        status = soup.find_all("p", {"data-issue-status": "icon"})
+        status_span = soup.find_all("span")
+        updated = soup.find_all("div", {"data-issue": "date"})
+        actions = soup.find_all("div", {"data-modal": "body"})
 
-print("Issue:", issue)
-print("Title:", title)
-print("Partners:", partners_li)
-print("Status:", status_span)
-print("Updated:", updated)
-print("Actions:", actions)
+        # Extract the text from the BeautifulSoup objects
+        issues_text = ', '.join([issue.text.strip() for issue in issues])
+        title_text = ', '.join([t.text.strip() for t in title])
+        partners_text = ', '.join([p.text.strip() for p in partners])
+        partners_li_text = ', '.join([li.text.strip() for li in partners_li])
+        status_text = ', '.join([s.text.strip() for s in status])
+        status_span_text = ', '.join([sp.text.strip() for sp in status_span])
+        updated_text = ', '.join([u.text.strip() for u in updated])
+        actions_text = ', '.join([a.text.strip() for a in actions])
+
+        # Write the extracted information to the CSV file
+        writer.writerow([issues_text, title_text, partners_text, status_text, updated_text, actions_text])
+
+print("Scraping complete.")
+
 
 
 
