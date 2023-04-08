7cddcf7b8730cc735139648eb03785ed740b33ef
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 19:22:55 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -16,7 +16,7 @@ divs = soup.find_all("div", {"data-card": "details"})
 for div in divs:
     issue = soup.find_all("p", {"data-issue-header": "index"})
     title = soup.find_all("h3", {"data-issue-header": "title"})
-    partners = soup.find_all("p", {"class": "extra-small-san-serif"})
+    partners = soup.find_all("p", {"ul": "li"})
 
 print("Issue:", issue)
 print("Title:", title)
