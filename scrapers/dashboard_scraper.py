93bcadcfb2cf6a4bd09edee2ad75efab9d7f3aac
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 22:03:52 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -26,13 +26,14 @@ with open('25_demands_table.csv', 'w', newline='') as csvfile:
         partners = []
         partners_div = div.find("div", {"data-issue": "partners"})
         partners = [li.text.strip() for li in partners_div.find_all("li")]
-        #this status one needs help
         status = []
         status_p = div.find("p", {"data-issue-status": "icon"})
         status = [span.text.strip() for span in status_p.find_all("span")]
-        updated = []
-        for update in div.find_all("div", {"data-issue": "date"}):
-            updated.append(update.text.strip())
+        update = []
+        update_div = div.find("div", {"data-issue": "date"})
+        update = [time.text.strip() for time in update_div.find_all("time")]
+        #for update_div in div.find_all("div", {"data-issue": "date"}):
+        #    updated.append(update.text.strip())
         actions = []
         for action in div.find_all("div", {"data-modal": "body"}):
             actions.append(action.text.strip())
@@ -49,11 +50,14 @@ with open('25_demands_table.csv', 'w', newline='') as csvfile:
             status_p_text = ', '.join([span.text.strip() for span in status_p.find_all("span")])
         #status_text = ', '.join([s.text.strip() for s in status])
         #status_span_text = ', '.join([sp.text.strip() for sp in status_span])
-        updated_text = ', '.join([update.strip() for update in updated])
+        update_div_text = ''
+        if update_div is not None:
+            update_div_text = ', '.join([time.text.strip() for time in update_div.find_all("time")])
+        #updated_text = ', '.join([update.strip() for update in updated])
         actions_text = ', '.join([action.strip() for action in actions])
 
         # Write the extracted information to the CSV file
-        writer.writerow([issues_text, titles_text, partners_div_text, status_p_text, updated_text, actions_text])
+        writer.writerow([issues_text, titles_text, partners_div_text, status_p_text, update_div_text, actions_text])
 
 print("Scraping complete.")
 
