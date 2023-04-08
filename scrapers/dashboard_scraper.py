603019adc14ac6ee5d4bea1e7f3f04ada99d5782
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 23:35:53 +0000

    actions

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -17,49 +17,56 @@ with open('25_demands_table.csv', 'w', newline='') as csvfile:
     writer.writerow(['Issue', 'Title', 'Partner', 'Status', 'Updated', 'Actions'])
 
     for div in divs:
+        # issue loop
         issues = []
         for issue in div.find_all("p", {"data-issue-header": "index"}):
             issues.append(issue.text.strip())
+        # title loop
         titles = []
         for title in div.find_all("h3", {"data-issue-header": "title"}):
             titles.append(title.text.strip())
-        actions = []
-        actions_umd_modal = div.find("umd-modal", {"data-card": "modal"})
-        if actions_umd_modal is not None:
-            actions = [p.text.strip() for p in actions_umd_modal.find_all("p")]
-
-        actions_umd_modal_text = ''
-        if actions_umd_modal is not None:
-            actions_umd_modal_text = ', '.join([p.text.strip() for p in actions_umd_modal.find_all("p")])
-        
-        #actions_text = ', '.join([action.strip() for action in actions])
+        # partner loop
         partners = []
         partners_div = div.find("div", {"data-issue": "partners"})
         partners = [li.text.strip() for li in partners_div.find_all("li")]
+        #status loop
         status = []
         status_p = div.find("p", {"data-issue-status": "icon"})
         status = [span.text.strip() for span in status_p.find_all("span")]
+        # update loop
         update = []
         update_div = div.find("div", {"data-issue": "date"})
         update = [time.text.strip() for time in update_div.find_all("time")]
+        # action loop
+        actions = []
+        action_umd_modal = div.find("div", {"data-modal": "body"})
+        #if action_umd_modal is not None:
+        actions = [p.text.strip() for p in action_umd_modal.find_all("p")]
 
+        # now we joining shit so that we just got that text
 
-
+        # issues join
         issues_text = ', '.join([issue.strip() for issue in issues])
+        # titles join
         titles_text = ', '.join([title.strip() for title in titles])
+        # partners join
         partners_div_text = ''
         if partners_div is not None:
             partners_div_text = ', '.join([li.text.strip() for li in partners_div.find_all("li")])
+        # status join
         status_p_text = ''
         if status_p is not None:
             status_p_text = ', '.join([span.text.strip() for span in status_p.find_all("span")])
+        # update join
         update_div_text = ''
         if update_div is not None:
             update_div_text = ', '.join([time.text.strip() for time in update_div.find_all("time")])
-        #actions_text = ', '.join([action.strip() for action in actions])
-        #if actions_div is not None:
-            #actions_div_text = ', '.join([p.text.strip() for p in actions_div.find_all("p")])
+        # action join
+        action_umd_modal_text = ', '.join([p.strip() for  in actions])
+        if actions_div is not None:
+            actions_div_text = ', '.join([p.text.strip() for p in actions_div.find_all("p")])
 
+# write it out to csv
         writer.writerow([issues_text, titles_text, partners_div_text, status_p_text, update_div_text, actions_umd_modal_text])
 
 print("Scraping complete.")
