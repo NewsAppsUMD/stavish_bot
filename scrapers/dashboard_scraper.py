70445b4dc9357866d69bcc7e1995b75476c0ec6b
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 15:41:41 +0000

    first web scraper

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -11,29 +11,20 @@ from pprint import pprint
 url = 'https://diversity.umd.edu/black-student-leaders'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-
 data = requests.get(url)
 
-
-my_data = []
-
-
 html = BeautifulSoup(data.text, 'html.parser')
-articles = html.select('.critical_issues')
+
+datas = soup.find_all("details")
+
+# Iterate through all li tags
+for data in datas:
+    # Get text from each tag
+    print(data.text)
+ 
+print(f"Total {len(datas)} li tag found")
 
 
-for article in articles:
-
-
-    title = article.select('.header')[0].get_text()
-    excerpt = article.select('.small-san-serif')[0].get_text()
-    pub_date = article.select('.extra-small-san-serif')[0].get_text()
-
-
-    my_data.append({"title": title, "excerpt": excerpt, "pub_date": pub_date})
-
-
-pprint(my_data)
 
 
 
