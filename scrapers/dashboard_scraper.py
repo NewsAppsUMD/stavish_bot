ead8635e9e6104f81a486365946cd1eab25a05ee
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 18:17:00 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -6,11 +6,28 @@ from pprint import pprint
 
 url = 'https://diversity.umd.edu/black-student-leaders'
 response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = response.content
 
 soup = BeautifulSoup(html, features="html.parser")
-datas = soup.find('data-card="details"')
-print(soup.prettify())
+
+divs = soup.find_all("div", {"data-card": "details"})
+
+# Loop through each div tag and extract the desired information
+for div in divs:
+    # Extract the title
+    title = div.find("h3").text.strip()
+
+    # Extract the description
+    description = div.find("div", {"class": "desc"}).text.strip()
+
+    # Extract the image URL
+    img_url = div.find("img")["src"]
+
+    # Print out the extracted information
+    print("Title:", title)
+    print("Description:", description)
+    print("Image URL:", img_url)
+    print("\n")
+
 
 
 
