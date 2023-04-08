0bf148f5f5cf763305835f4a706817d9bc09b3fd
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 19:40:37 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -18,13 +18,12 @@ for div in divs:
     title = soup.find_all("h3", {"data-issue-header": "title"})
     partners = soup.find_all("div", {"data-issue": "partners"})
     partners_li = soup.find_all("ul")
-    status = soup.find_all("p", {"data-issue-status": "icon"})
-    status_span = soup.find_all("span")
+    status = soup.find_all("p", {"data-issue-status": "icon"}).find_all("span")
 
 print("Issue:", issue)
 print("Title:", title)
 print("Partners:", partners_li)
-print("Status:", status_span)
+print("Status:", status)
 
 
 
