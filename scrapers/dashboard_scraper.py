e56a921e272ee22988f64ecddf15a2295634ecac
Victoria Stavish <Vstavish@terpmail.umd.edu>
2023-04-08 21:13:17 +0000

    pretty with beautiful soup

--- scrapers/dashboard_scraper.py
+++ scrapers/dashboard_scraper.py
@@ -12,18 +12,18 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 divs = soup.find_all("div", {"data-card": "details"})
 
-    for div in divs:
-        issue = ("p", {"data-issue-header": "index"}):
-            all_issue = [issue.text for issue in issue]
-            all_issue[0:25]
-        title = soup.find_all("h3", {"data-issue-header": "title"})
-        partners = soup.find_all("div", {"data-issue": "partners"})
-        partners_li = soup.find_all("ul")
-        #this status one needs help
-        status = soup.find_all("p", {"data-issue-status": "icon"})
-        status_span = soup.find_all("span")
-        updated = soup.find_all("div", {"data-issue": "date"})
-        actions = soup.find_all("div", {"data-modal": "body"})
+for div in divs:
+    issue = ("p", {"data-issue-header": "index"}):
+        all_issue = [issue.text for issue in issue]
+        all_issue[0:25]
+    title = soup.find_all("h3", {"data-issue-header": "title"})
+    partners = soup.find_all("div", {"data-issue": "partners"})
+    partners_li = soup.find_all("ul")
+    #this status one needs help
+    status = soup.find_all("p", {"data-issue-status": "icon"})
+    status_span = soup.find_all("span")
+    updated = soup.find_all("div", {"data-issue": "date"})
+    actions = soup.find_all("div", {"data-modal": "body"})
 
 
 print("Issue:", issue)
